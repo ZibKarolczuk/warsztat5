de0d59f50296e71629dbe6c7e1dd229baee57433
Zbigniew Karolczuk <karolczuk.z@gmail.com>
2018-08-03 14:59:27 +0200

    Add Book model and BookController

--- src/main/java/pl/coderslab/warsztat5/controller/HomeController.java
+++ src/main/java/pl/coderslab/warsztat5/controller/HomeController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class TestController {
+public class HomeController {
 
-    @GetMapping
+    @GetMapping("")
     @ResponseBody
-    public String test(){
-        return "test konfiguracji projektu ąóąćęĘŻ";
+    public String home(){
+        return "Server ready";
     }
 
 }
